b5178c2c52f298f8ecdba739827537e273422959
Dinuka1220 <earnwithdinu1220@gmail.com>
2025-03-21 14:54:22 +0530

    git init

---
+++ src/Componants/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='bg-green-300 items-center justify-center'>
+      my header
+    </div>
+  )
+}
+
+export default Header
